73bae8209a308657d68bfea190b31d402b89a292
razzaksr <razzaksr@gmail.com>
2025-03-24 19:38:00 +0530

    JSON CRD done

--- scripts/bank-crud.js
+++ scripts/bank-crud.js
@@ -1,33 +1,50 @@
-let banks = [
-    {
-        "name":"icici",
-        "branch":"ponnammapet",
-        "address":"Salem",
-        "ifscode":"icic3434c",
-        "contact":7656453546
-    },
-    {
-        "name":"axis",
-        "branch":"Rasipuram",
-        "address":"Namakkal",
-        "ifscode":"uti3565c",
-        "contact":8765434567
-    },
-    {
-        "name":"indian bank",
-        "branch":"tiruchengode",
-        "address":"Namakkal",
-        "ifscode":"idib67567r",
-        "contact":87654567865
-    }
-]
+let banks = []
 
+// const initialize = () =>{
+//     localStorage.setItem('bankings',JSON.stringify(banks))
+// }
+
+// delete
+const deletes = (position) =>{
+    banks = JSON.parse(localStorage.getItem('bankings'))
+    alert('Bank to be deleted @ '+position)
+    // collect all except given position
+    banks = banks.filter((value, index)=>{
+        return index!=position
+    })
+    localStorage.setItem('bankings',JSON.stringify(banks))
+    window.location.assign('http://127.0.0.1:5500/BootstrapDemo/Alignments.html')
+}
+
+// create
+const NewEntry = () =>{
+    // load from local storage
+    banks = JSON.parse(localStorage.getItem('bankings'))
+    const myJson = {
+        "name":document.getElementById('bank-name').value,
+        "branch":document.getElementById('bank-branch').value,
+        "address":document.getElementById('bank-address').value,
+        "ifscode":document.getElementById('bank-ifsc').value,
+        "contact":document.getElementById('bank-contact').value
+    }
+    banks.push(myJson)
+    alert('new bank entry has added')
+    document.getElementById('bank-name').value=""
+    document.getElementById('bank-branch').value=""
+    document.getElementById('bank-address').value=""
+    document.getElementById('bank-ifsc').value=""
+    document.getElementById('bank-contact').value=""
+    // write string to local storage
+    localStorage.setItem('bankings',JSON.stringify(banks))
+}
+// read
 const view=()=>{
     var tableBody = document.getElementsByTagName("tbody")[0]
     var content = ""
+    banks = JSON.parse(localStorage.getItem('bankings'))
     banks.map((value,index)=>{
         // alert(JSON.stringify(value))
-        content+="<tr><td>"+value.name+"</td><td>"+value.address+"</td><td>"+value.ifscode+"</td><td>"+value.contact+"</td><td>"+value.branch+"</td></tr>"
+        content+="<tr><td>"+value.name+"</td><td>"+value.address+"</td><td>"+value.ifscode+"</td><td>"+value.contact+"</td><td onclick='deletes("+index+")'>"+value.branch+"</td></tr>"
     })
     tableBody.innerHTML = content
 }
